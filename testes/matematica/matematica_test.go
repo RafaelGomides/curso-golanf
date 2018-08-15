97572e8adb66bd1cca0df777139c3f9858b1aee8
Rafael Gomides <mrrafagomides@gmail.com>
2018-08-15 20:27:05 -0300

    Alguns testes

---
+++ testes/matematica/matematica_test.go
@@ -0,0 +1,14 @@
+package matematica
+
+import "testing"
+
+const erroPadrao = "Valor esperado %v, mas o resultado foi %v"
+
+func TestMedia(t *testing.T) {
+	valorEsperado := 6.45
+	valor := Media(7.3, 9.2, 3.4, 5.9)
+
+	if valor != valorEsperado {
+		t.Errorf(erroPadrao, valorEsperado, valor)
+	}
+}
