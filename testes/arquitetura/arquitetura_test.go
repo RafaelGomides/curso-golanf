f141241884c3fe424b380e41bac81f0570621223
Rafael Gomides <mrrafagomides@gmail.com>
2018-08-15 20:35:22 -0300

    Finalização de testes

--- testes/arquitetura/arquitetura_test.go
+++ testes/arquitetura/arquitetura_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestDependente(t *testing.T) {
+	t.Parallel()
 	if runtime.GOARCH == "amd64" {
 		t.Skip("Não funciona em amd64")
 	}
--- testes/matematica/matematica_test.go
+++ testes/matematica/matematica_test.go
@@ -5,6 +5,7 @@ import "testing"
 const erroPadrao = "Valor esperado %v, mas o resultado foi %v"
 
 func TestMedia(t *testing.T) {
+	t.Parallel()
 	valorEsperado := 6.45
 	valor := Media(7.3, 9.2, 3.4, 5.9)
 
